d983709c9b45b3cb20671d2d5b25b5eafbae583b
Jordan Sheppard <jg.sheppard808@gmail.com>
2025-11-07 12:10:16 -0700

    Changed build_design.m to have a column for the t term in the project spec

--- src/build_design.m
+++ src/build_design.m
@@ -11,25 +11,31 @@ function [A,b,meta] = build_design(y, s, N, K)
 %
     y = y(:); T = numel(y);
     M = T - N;          % Number of time periods in response vector
-    p = 1 + N + 2*K;    % Number of terms in time series linear equation 
+    p = 2 + N + 2*K;    % Number of terms in time series linear equation 
     if M < p
-        error('Underdetermined: T-N (= %d) must exceed p (= %d).', M, p);
+        error('Underdetermined: T-N (= %d) must be >= 2K+N+2 (= %d).', M, p);
     end
     b = y(N+1:T);
+    t = (N+1:T).';
     A = ones(M, p);
-    col = 1;
-    % lag columns: 2 through 2+(N-1) = N+1
+
+    % First column: all ones
+    % Second column: t's
+    A(:,2) = t;
+
+    % lag columns: 3 through 3+(N-1) = N+2
+    col = 2;
     for i = 1:N
         col = col + 1;
         A(:, col) = y(N+1-i : T-i);
     end
-    t = (N+1:T).';
-    % cosine columns: N+2 through N+2+(K-1)=N+K+1
+    
+    % cosine columns: N+3 through N+3+(K-1)=N+K+2
     for k = 1:K
         col = col + 1;
         A(:, col) = cos(2*pi*k*t/s);
     end
-    % sine columns: N+K+2 through N+K+2+(K-1) = N+2K+1 = p
+    % sine columns: N+K+3 through N+K+3+(K-1) = N+2K+2 = p
     for k = 1:K
         col = col + 1;
         A(:, col) = sin(2*pi*k*t/s);
