f83cdee7498557dd5651f31b0b316d44b1f7cbec
Jordan Sheppard <jg.sheppard808@gmail.com>
2025-11-07 11:56:21 -0700

    Commented build_design.m to better understand structure of design matrix

--- src/build_design.m
+++ src/build_design.m
@@ -10,25 +10,26 @@ function [A,b,meta] = build_design(y, s, N, K)
 %   meta : struct with fields: .rows=M, .p=p, .t=(N+1:T).'
 %
     y = y(:); T = numel(y);
-    M = T - N; p = 1 + N + 2*K;
+    M = T - N;          % Number of time periods in response vector
+    p = 1 + N + 2*K;    % Number of terms in time series linear equation 
     if M <= p
         error('Underdetermined: T-N (= %d) must exceed p (= %d).', M, p);
     end
     b = y(N+1:T);
     A = ones(M, p);
     col = 1;
-    % lag columns
+    % lag columns: 2 through 2+(N-1) = N+1
     for i = 1:N
         col = col + 1;
         A(:, col) = y(N+1-i : T-i);
     end
     t = (N+1:T).';
-    % cosine columns
+    % cosine columns: N+2 through N+2+(K-1)=N+K+1
     for k = 1:K
         col = col + 1;
         A(:, col) = cos(2*pi*k*t/s);
     end
-    % sine columns
+    % sine columns: N+K+2 through N+K+2+(K-1) = N+2K+1 = p
     for k = 1:K
         col = col + 1;
         A(:, col) = sin(2*pi*k*t/s);
