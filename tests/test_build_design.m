75df574cc035cc218921004d049530ffb877ee51
Jordan Sheppard <jg.sheppard808@gmail.com>
2025-11-07 14:03:50 -0700

    Modified test_build_design.m to account for the extra column for the linear term in the assignment

--- tests/test_build_design.m
+++ tests/test_build_design.m
@@ -1,6 +1,6 @@
 addpath(fullfile('..','src'));
 y = (1:20)'; s=12; N=3; K=2;
 [A,b,meta] = build_design(y,s,N,K);
-assert(size(A,1)==20-3 && size(A,2)==1+3+2*2);
+assert(size(A,1)==20-3 && size(A,2)==2+3+2*2);  % CHANGED TO ADD LINEAR TERM!
 assert(all(b(1:3)==y(4:6)));
 disp('build_design OK');
