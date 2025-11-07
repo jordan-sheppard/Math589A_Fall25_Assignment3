b27fabdee9bc29ea7a145d80d917c239d731ebd9
Jordan Sheppard <jg.sheppard808@gmail.com>
2025-11-07 13:14:38 -0700

    Modified and commented unpack_coeffs.m to unpack all coefficients, including the d coefficient on the linear term that was not included before

--- src/unpack_coeffs.m
+++ src/unpack_coeffs.m
@@ -1,6 +1,24 @@
 function coef = unpack_coeffs(beta, N, K)
+% UNPACK_COEFFS  Unpacks and sorts best-fit coefficients for the
+% N-th order difference eq (with K seasonal harmonics).
+%
+%   beta : (2K+N+2)x1 double; coefficients of best fit
+%   N : nonnegative integer; number of lag terms
+%   K : nonnegative integer; number of seasonal harmonics
+%
+% Returns:
+%   coef: struct with fields:
+%       .c: Scalar; constant term
+%       .d: Scalar; coefficient of time index 
+%       .a: Length N vector; coefficients of lag
+%       .alpha: Length K vector; coefficients of cosine
+%               seasonal harmonics
+%       .beta: Length K vector; coefficients of sine
+%              seasonal harmonics
+%
     coef.c = beta(1);
-    coef.a = beta(2:1+N);
-    coef.alpha = beta(1+N+1 : 1+N+K);
-    coef.beta  = beta(1+N+K+1 : 1+N+2*K);
+    coef.d = beta(2);
+    coef.a = beta(3:N+2);
+    coef.alpha = beta(N+2+1 : N+K+2);
+    coef.beta  = beta(N+K+2+1 : N+2*K+2);
 end
